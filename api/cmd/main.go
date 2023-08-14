c67156d5c34862925a5ee3bf77374480623ccb8f
Bill Wordsworth <billwor1234@gmail.com>
2023-08-14 17:26:36 -0400

    Add server connection logic

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"net/http"
+	"script_sight/server"
+)
+
+const port = ":8080"
+
 func main() {
+	s := &http.Server{Addr: port}
+
+	// Initialize the server with configuration.
+	srv := server.New(s)
+
+	// Ensure routes are registered before starting the server.
+	srv.RegisterRoutes()
+
+	// Start the server
+	srv.ListenAndServe()
 }
