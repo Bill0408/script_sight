0a58d30c8f7d6fda5793e7a05be91aaacb671bfc
Bill Wordsworth <billwor1234@gmail.com>
2023-08-15 18:11:52 -0400

    Add createImgDir()

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/http"
+	"os"
 	"script_sight/server"
 )
 
-const port = ":8080"
+const (
+	port = ":8080"
+
+	dirName  = "../script_sight/api/controller/img"
+	ownerRWS = 0700
+)
 
 func main() {
+	createImgDir()
+
 	s := &http.Server{Addr: port}
 
 	// Initialize the server with configuration.
@@ -18,4 +28,15 @@ func main() {
 
 	// Start the server
 	srv.ListenAndServe()
+}
+
+// createImgDir creates the img directory in the controller directory
+// that will be used to temporarily store images uploaded to the server.
+func createImgDir() {
+	err := os.Mkdir(dirName, ownerRWS)
+	if err != nil {
+		if !errors.Is(err, os.ErrExist) {
+			log.Fatal(err)
+		}
+	}
 }
