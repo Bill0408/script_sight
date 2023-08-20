b9a61feae70bd13cd0ccb94dbafd7a92eea9d78c
Bill Wordsworth <billwor1234@gmail.com>
2023-08-20 10:55:20 -0400

    Add file hash for home.js

--- api/controller/home.go
+++ api/controller/home.go
@@ -1,20 +1,29 @@
 package controller
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
-func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+type data struct {
+	Hash string
+}
+
+func (c *Controller) HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	d := data{Hash: c.Hash}
+
 	// Create a new html template from the home.html file.
 	t, err := template.ParseFiles("../script_sight/frontend/pages/home.html")
 	if err != nil {
+		fmt.Println(err)
 		http.Error(w, "An error occurred parsing the homepage file.", http.StatusInternalServerError)
 		return
 	}
 
 	// Send back the parsed home.html page.
-	if err = t.Execute(w, nil); err != nil {
+	if err = t.Execute(w, d); err != nil {
+		fmt.Println(err)
 		http.Error(w, "An error occurred executing the template.", http.StatusInternalServerError)
 		return
 	}
