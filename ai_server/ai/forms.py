968aa3cac1ecef70afb13c9aa2e3542316493640
Bill Wordsworth <billwor1234@gmail.com>
2023-08-20 10:54:10 -0400

    Replace image with uploadfile

--- ai_server/ai/forms.py
+++ ai_server/ai/forms.py
@@ -4,5 +4,5 @@ from django import forms
 # ImageUploadForm represents a Django form used for uploading images.
 # It inherits from Django's Form class.
 class ImageUploadForm(forms.Form):
-    # Declaring an ImageField named 'image' to handle image uploads.
-    image = forms.ImageField()
+    # Declaring an ImageField named 'uploadFile' to handle image uploads.
+    uploadFile = forms.ImageField()
