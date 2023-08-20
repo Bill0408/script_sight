3e6705f9379aba1f52d4dc9e698a15faccc3770d
Bill Wordsworth <billwor1234@gmail.com>
2023-08-20 10:57:57 -0400

    Add file hashing function

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/sha256"
 	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"script_sight/controller"
 	"script_sight/server"
 )
 
@@ -18,10 +22,12 @@ const (
 func main() {
 	createImgDir()
 
+	c := controller.New(hashFile("../script_sight/frontend/static/home.js"))
+
 	s := &http.Server{Addr: port}
 
 	// Initialize the server with configuration.
-	srv := server.New(s)
+	srv := server.New(s, c)
 
 	// Ensure routes are registered before starting the server.
 	srv.RegisterRoutes()
@@ -39,4 +45,30 @@ func createImgDir() {
 			log.Fatal(err)
 		}
 	}
+}
+
+// hashFile takes in a filepath and creates a hash of the file's content.
+func hashFile(path string) string {
+	file, err := os.Open(path) // Open the file at the specified path.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Release resources when hashfile returns.
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			os.Exit(1)
+		}
+	}(file)
+
+	hash := sha256.New() // Get a new hash computing the SHA256 checksum.
+	// Copy the content from the file into the hash.
+	if _, err = io.Copy(hash, file); err != nil {
+		log.Fatal(err)
+	}
+
+	hashInBytes := hash.Sum(nil)[:20]            // Get the hash.
+	hashString := fmt.Sprintf("%x", hashInBytes) // Convert the hash to string.
+
+	return hashString[0:20]
 }
