afe38ae29736e35e2ba50987278101bac45394fc
Bill Wordsworth <billwor1234@gmail.com>
2023-08-24 11:47:10 -0400

    Add modeling loading

---
+++ ai_server/ai/model_loading.py
@@ -0,0 +1,11 @@
+import torch
+from .model import Model
+
+# Use cuda if a gpu supporting cuda is available, if it isn't available, use the cpu.
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = Model()
+
+model_path = "../ai_server/ai/model_weights.pth"
+model.load_state_dict(torch.load(model_path, map_location=device))
+model.to(device).eval()  # Move to GPU if available and set to evaluation mode
